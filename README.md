3c91db22c8fb81885f13db2c6f225ef935c5ab9f
Clément Dumas <clement.dumas@ens-paris-saclay.fr>
2024-02-10 18:00:36 +0100

    updated data generation

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 ## Installation
 
 ```bash
-git submodule init
-git submodule update
 conda create -n abs_cot
 conda activate abs_cot
 pip install -r requirements.txt
+```
+
+## Reproducing the results
+
+### Prompt generation:
+```bash
+git submodule init
+git submodule update
+python prompt_generation.py
 ```
--- prompt_generation.py
+++ prompt_generation.py
@@ -1,12 +1,3 @@
-from DecompositionFaithfulnessPaper.prompts.factored_decomposition_recomposition_few_shot_prompt import (
-    FD_RECOMPOSITION_FEW_SHOT as recomp_promt,
-)
-from DecompositionFaithfulnessPaper.prompts.factored_decomposition_decomposition_few_shot_prompt import (
-    FD_DECOMPOSITION_FEW_SHOT as decomp_promt,
-)
-import json
-
-
 def few_shot_to_messages(few_shot_prompt: str) -> list[dict[str, str]]:
     """
     Convert a few-shot prompt to a list of messages. The format of the few-shot prompt is as follows:
@@ -31,7 +22,17 @@ def few_shot_to_messages(few_shot_prompt: str) -> list[dict[str, str]]:
 
 
 if __name__ == "__main__":
+    from DecompositionFaithfulnessPaper.prompts.factored_decomposition_recomposition_few_shot_prompt import (
+        FD_RECOMPOSITION_FEW_SHOT as recomp_promt,
+    )
+    from DecompositionFaithfulnessPaper.prompts.factored_decomposition_decomposition_few_shot_prompt import (
+        FD_DECOMPOSITION_FEW_SHOT as decomp_promt,
+    )
+    import json
+
     decomp_promt = few_shot_to_messages(decomp_promt)
     recomp_promt = few_shot_to_messages(recomp_promt)
     with open("data/fd_few_shot_chat.json", "w") as f:
-        json.dump({"decomp_promt": decomp_promt, "recomp_promt": recomp_promt}, f, indent=4)
+        json.dump(
+            {"decomp_promt": decomp_promt, "recomp_promt": recomp_promt}, f, indent=4
+        )
